a70ce93f11f7348927a07e9d658aa86c55666b09
Mohamed <mohamedz.1@hotmail.com>
2017-05-05 09:23:46 +0200

    first

---
+++ UCLoadingButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UCLoadingButton
+//
+//  Created by Mohamed on 5/05/17.
+//  Copyright © 2017 B&D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
